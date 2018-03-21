d32eb54b7c868d780af6e255c5ab8018383b7a5a
Arvid Mildner <mildner.arvid@gmail.com>
2018-03-21 22:39:37 +0100

    Added default route to CV site

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 resources :people
 resources :cvs
 
-root to: "people#index"
 root to: "cvs#index"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
